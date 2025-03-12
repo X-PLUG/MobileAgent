91fb1ba9e763d6ab31e21f28edc73a2da0057f3c
zhangxi1997 <383781587@qq.com>
2025-03-12 13:43:08 +0800

    Update crop.py

--- PC-Agent/PCAgent/crop.py
+++ PC-Agent/PCAgent/crop.py
@@ -2,8 +2,6 @@ import math
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import clip
-import torch
 
 
 def crop_image(img, position):
@@ -119,23 +117,4 @@ def crop_for_clip(image, box, i, position):
         return True
     else:
         return False
-    
-    
-def clip_for_icon(clip_model, clip_preprocess, images, prompt):
-    image_features = []
-    for image_file in images:
-        image = clip_preprocess(Image.open(image_file)).unsqueeze(0).to(next(clip_model.parameters()).device)
-        image_feature = clip_model.encode_image(image)
-        image_features.append(image_feature)
-    image_features = torch.cat(image_features)
-    
-    text = clip.tokenize([prompt]).to(next(clip_model.parameters()).device)
-    text_features = clip_model.encode_text(text)
 
-    image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarity = (100.0 * image_features @ text_features.T).softmax(dim=0).squeeze(0)
-    _, max_pos = torch.max(similarity, dim=0)
-    pos = max_pos.item()
-    
-    return pos
